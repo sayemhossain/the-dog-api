570f56c4957ae7fd9c1f8d22af2372d38193170f
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-02-27 14:41:11 +0600

    new code added

--- script.js
+++ script.js
@@ -8,10 +8,20 @@ const displayDog = (dogList) => {
   const firstTenDog = dogList.slice(0, 10);
   for (const dog of firstTenDog) {
     const div = document.createElement("div");
-    div.classList.add("col-lg-4");
+    div.classList.add("col");
     div.innerHTML = `
-        <h2>${dog.name}</h2>
-    `;
+  <div onclick="loadMealDetail(${dog.id})" class="card mx-auto my-4" style=" width:22rem">
+          <img src="${dog.image.url}" class="card-img-top img-fluid" alt="...">
+          <div class="card-body">
+            <h4 class="card-title text-primary">${dog.name}</h4>
+            <p>Temperament: ${dog.temperament}</p>
+            <p>Bred for: ${dog.bred_for}</p>
+            <p>Origin: ${dog.origin}</p>
+            <p>Life span: ${dog.life_span}</p>
+            <button class="btn btn-warning">Read More</button>
+          </div>
+  </div>
+  `;
     dogDetails.appendChild(div);
   }
 };
